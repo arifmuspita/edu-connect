c61a0fb8b565955d44815194b6c5d6b90802830f
amuspita <amuspita@gmail.com>
2025-03-27 17:11:22 +0700

    fix:host swagger part 3

--- user-service/docs/docs.go
+++ user-service/docs/docs.go
@@ -345,7 +345,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://user-service-639370343880.asia-southeast1.run.app",
+	Host:             "user-service-639370343880.asia-southeast1.run.app",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "User Service API",
